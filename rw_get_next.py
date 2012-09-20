7f539e11fd14a736fccc7fcee8f26ff006057c18
Rob McAuley <rmcauley@rainwave.cc>
2012-09-20 16:07:55 -0400

    get_next.py actually fetches data!

--- rw_get_next.py
+++ rw_get_next.py
@@ -7,14 +7,14 @@ import httplib
 import urllib
 import argparse
 
-parser = argparse.ArgumentParser(description="Rainwave backend daemon.")
-parser.add_argument("--host", "-h", required=True)
-parser.add_argument("--port", "-p", required=True)
+parser = argparse.ArgumentParser(description="Fetches the next song from a Rainwave backend daemon.")
+parser.add_argument("--dest", "-d", required=False, default="127.0.0.1")
+parser.add_argument("--port", "-p", required=True, type=int)
 parser.add_argument("--sid", "-s", required=True)
 args = parser.parse_args()
 
-params = urllib.urlencode({ "sid": sid })
-conn = httplib.HTTPConnection('localhost', args.host, args.port)
+params = urllib.urlencode({ "sid": args.sid })
+conn = httplib.HTTPConnection(args.dest, args.port)
 conn.request("GET", "/advance/%s" % args.sid)
 result = conn.getresponse()
 print result.read()
