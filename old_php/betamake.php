322145d1c981f03ba6d9b4cdac90677b96d4b5f5
Rob McAuley <rmcauley@rainwave.cc>
2012-12-31 14:59:42 -0500

    Preparing for merge with Python project.

--- old_php/betamake.php
+++ old_php/betamake.php
@@ -9,11 +9,7 @@ if (isset($_SERVER['argv'][1])) {
 }
 
 $dest = "/var/www/rainwave.cc/beta/";
-if (gethostname() == "substream") {
-	$dest = "/home/rmcauley/r3/beta/";
-}
 $bnum = getBuildNumber();
-$lyredir = "/home/rmcauley/lyre/";
 $langstatus = true;
 $cookiedomain = 'rainwave.cc';
 
@@ -23,7 +19,6 @@ removeOldBuild($dest);
 copyStatic($dest, $bnum);
 buildLanguages($dest, $bnum);
 buildBetaSkins($dest, $bnum);
-copyFile($lyredir . "javascript/lyre-ajax.js", "lyre-ajax.js", $dest);
 copyDirectory("js", "js", $dest);
 writeParsedFile("root/beta-index.php", $dest . "index.php", $bnum);
 copyFile("files.php", "files.php", $dest);
