b7187fa5e79b4a18c1482cb56af35cd0d528d3f4
Rob McAuley <rmcauley@rainwave.cc>
2013-07-09 23:03:14 +0900

    Add album_rating_complete functionality

--- rainwave/rating.py
+++ rainwave/rating.py
@@ -56,18 +56,23 @@ def set_song_rating(song_id, user_id, rating = None, fave = None):
 	
 	toreturn = []
 	for album_id in db.c.fetch_list("SELECT album_id FROM r4_song_album WHERE song_id = %s", (song_id,)):
-		album_rating = float(db.c.fetch_var("SELECT ROUND(CAST(AVG(song_rating_user) AS NUMERIC), 1) AS user_rating FROM r4_song_album JOIN r4_song_ratings ON (album_id = %s AND r4_song_album.song_id = r4_song_ratings.song_id) "
-									  "WHERE user_id = %s",
-									  (album_id, user_id)))
-		toreturn.append({ "id": album_id, "user_rating": album_rating})
+		user_data = db.c.fetch_row("SELECT ROUND(CAST(AVG(song_rating_user) AS NUMERIC), 1) AS user_rating, COUNT(song_rating_user) AS user_rating_count FROM r4_song_album JOIN r4_song_ratings ON (album_id = %s AND r4_song_album.song_id = r4_song_ratings.song_id) "
+							  "WHERE user_id = %s",
+							  (user_id, user_id))
+		num_songs = db.c.fetch_var("SELECT COUNT(song_id) FROM r4_song_album WHERE album_id = %s", (album_id,))
+		rating_complete = False
+		if user_data['user_rating_count'] >= num_songs:
+			rating_complete = True
+		album_rating = float(album_data['user_rating'])
+		toreturn.append({ "id": album_id, "user_rating": album_rating, "rating_complete": rating_complete })
 		album_fave = False
 		existing_rating = db.c.fetch_row("SELECT album_rating_user, album_fave FROM r4_album_ratings WHERE album_id = %s AND user_id = %s", (album_id, user_id))
 		if existing_rating:
 			album_fave = existing_rating['album_fave']
-			db.c.update("UPDATE r4_album_ratings SET album_rating_user = %s, album_fave = %s WHERE user_id = %s AND album_id = %s",
-						(album_rating, album_fave, user_id, album_id))
+			db.c.update("UPDATE r4_album_ratings SET album_rating_user = %s, album_fave = %s, album_rating_complete = %s WHERE user_id = %s AND album_id = %s",
+						(album_rating, album_fave, rating_complete, user_id, album_id))
 		else:
-			db.c.update("INSERT INTO r4_album_ratings (album_rating_user, album_fave, user_id, album_id) VALUES (%s, %s, %s, %s)",
-						(album_rating, album_fave, user_id, album_id))
-		cache.set_album_rating(album_id, user_id, { "user_rating": album_rating, "fave": album_fave })
+			db.c.update("INSERT INTO r4_album_ratings (album_rating_user, album_fave, album_rating_complete, user_id, album_id) VALUES (%s, %s, %s, %s, %s)",
+						(album_rating, album_fave, rating_complete, user_id, album_id))
+		cache.set_album_rating(album_id, user_id, { "user_rating": album_rating, "fave": album_fave, "rating_complete": rating_complete })
 	return toreturn
