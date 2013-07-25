fdca3051c04ae96bd1f95e8c12ec385ce1b9fb9b
Rob McAuley <rmcauley@rainwave.cc>
2013-07-25 14:39:49 +0900

    Fix request panel not loading properly

--- static/js/panels/RequestsPanel.js
+++ static/js/panels/RequestsPanel.js
@@ -11,6 +11,23 @@ panels.RequestsPanel = {
 	constructor: function(container) {
 		var that = {};
 		var list = RequestList(true);
+		var latest_json = null;
+		
+		for (var i = 0; i < INITIAL_PAYLOAD.length; i++) {
+			for (var j in INITIAL_PAYLOAD[i]) {
+				if (j == "requests") {
+					latest_json = INITIAL_PAYLOAD[i][j];
+				}
+			}
+		}
+		
+		var latest_json_update = function(json) {
+				latest_json = json;
+		};
+		lyre.addCallback(latest_json_update, "requests");
+		
+		
+
 		//var line = AllRequestList();
 		
 		that.container = container;
@@ -21,8 +38,9 @@ panels.RequestsPanel = {
 			
 			that.el.appendChild(list.el);
 			//that.el.appendChild(line.div);
-			
 			lyre.addCallback(list.update, "requests");
+			list.update(latest_json);
+
 			//lyre.addCallback(line.update, "request_line");
 			
 			help.addStep("managingrequests", { "h": "managingrequests", "p": "managingrequests_p", "skipf": function() { edi.openPanelLink(false, "requests"); } });
