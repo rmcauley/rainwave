e04c3c58cd4683a42b863962088de1af8e95e935
Rob McAuley <rmcauley@rainwave.cc>
2013-11-20 22:54:13 +0900

    Fix random song selection

--- rainwave/playlist.py
+++ rainwave/playlist.py
@@ -119,18 +119,23 @@ def get_random_song_timed(sid, target_seconds = None, target_delta = 30):
 	if not target_seconds:
 		return get_random_song(sid)
 
-	sql_query = ("FROM r4_songs JOIN r4_song_sid USING (song_id) JOIN r4_album_sid USING (album_id) "
-				"WHERE r4_song_sid.sid = %s AND r4_album_sid.sid = %s AND "
-					"song_cool = FALSE AND song_elec_blocked = FALSE AND album_requests_pending != TRUE "
-					"AND song_request_only = FALSE AND song_length >= %s AND song_length <= %s")
-	num_available = db.c.fetch_var("SELECT COUNT(r4_song_sid.song_id) " + sql_query, (sid, sid, (target_seconds - (target_delta / 2)), (target_seconds + (target_delta / 2))))
+	sql_query = ("FROM r4_songs "
+					"JOIN r4_song_sid USING (song_id) "
+					"JOIN r4_album_sid ON (r4_album_sid.album_id = r4_song_sid.album_id AND r4_album_sid.sid = r4_song_sid.sid) "
+				"WHERE r4_song_sid.sid = %s "
+					"AND song_cool = FALSE "
+					"AND song_elec_blocked = FALSE "
+					"AND album_requests_pending IS NULL "
+					"AND song_request_only = FALSE "
+					"AND song_length >= %s AND song_length <= %s")
+	num_available = db.c.fetch_var("SELECT COUNT(r4_song_sid.song_id) " + sql_query, (sid, (target_seconds - (target_delta / 2)), (target_seconds + (target_delta / 2))))
 	if num_available == 0:
 		log.info("song_select", "No songs available with target_seconds %s and target_delta %s." % (target_seconds, target_delta))
 		log.debug("song_select", "Song select query: SELECT COUNT(r4_song_sid.song_id) %s" % (sql_query % (sid, sid, (target_seconds - (target_delta / 2)), (target_seconds + (target_delta / 2)))))
 		return get_random_song(sid)
 	else:
 		offset = random.randint(1, num_available) - 1
-		song_id = db.c.fetch_var("SELECT r4_song_sid.song_id " + sql_query + " LIMIT 1 OFFSET %s", (sid, sid, (target_seconds - target_delta), (target_seconds + target_delta), offset))
+		song_id = db.c.fetch_var("SELECT r4_song_sid.song_id " + sql_query + " LIMIT 1 OFFSET %s", (sid, (target_seconds - target_delta), (target_seconds + target_delta), offset))
 		return Song.load_from_id(song_id, sid)
 
 def get_random_song(sid):
@@ -139,16 +144,21 @@ def get_random_song(sid):
 	availability rules.  Falls back to get_random_ignore_requests on failure.
 	"""
 
-	sql_query = ("FROM r4_song_sid JOIN r4_album_sid USING (album_id) "
-		"WHERE r4_song_sid.sid = %s AND r4_album_sid.sid = %s AND song_cool = FALSE AND song_request_only = FALSE AND song_elec_blocked = FALSE AND album_requests_pending != TRUE")
-	num_available = db.c.fetch_var("SELECT COUNT(song_id) " + sql_query, (sid, sid))
+	sql_query = ("FROM r4_song_sid "
+					"JOIN r4_album_sid ON (r4_album_sid.album_id = r4_song_sid.album_id AND r4_album_sid.sid = r4_song_sid.sid) "
+				"WHERE r4_song_sid.sid = %s "
+					"AND song_cool = FALSE "
+					"AND song_request_only = FALSE "
+					"AND song_elec_blocked = FALSE "
+					"AND album_requests_pending IS NULL")
+	num_available = db.c.fetch_var("SELECT COUNT(song_id) " + sql_query, (sid,))
 	offset = 0
 	if num_available == 0:
 		log.info("song_select", "No songs available while ignoring request blocking rules.")
 		return get_random_song_ignore_requests(sid)
 	else:
 		offset = random.randint(1, num_available) - 1
-		song_id = db.c.fetch_var("SELECT song_id " + sql_query + " LIMIT 1 OFFSET %s", (sid, sid, offset))
+		song_id = db.c.fetch_var("SELECT song_id " + sql_query + " LIMIT 1 OFFSET %s", (sid, offset))
 		return Song.load_from_id(song_id, sid)
 
 def get_random_song_ignore_requests(sid):
@@ -156,7 +166,11 @@ def get_random_song_ignore_requests(sid):
 	Fetch a random song abiding by election block and availability rules,
 	but ignoring request blocking rules.
 	"""
-	sql_query = "FROM r4_song_sid WHERE r4_song_sid.sid = %s AND song_cool = FALSE AND song_elec_blocked = FALSE AND song_request_only = FALSE"
+	sql_query = ("FROM r4_song_sid "
+			"WHERE r4_song_sid.sid = %s "
+				"AND song_cool = FALSE "
+				"AND song_request_only = FALSE "
+				"AND song_elec_blocked = FALSE ")
 	num_available = db.c.fetch_var("SELECT COUNT(song_id) " + sql_query, (sid,))
 	offset = 0
 	if num_available == 0:
@@ -807,7 +821,6 @@ class AssociatedMetadata(object):
 		return False
 
 	def start_election_block(self, sid, num_elections = False):
-		print "***** %s" % self.elec_block
 		if self.elec_block is not None:
 			if self.elec_block > 0:
 				log.debug("elec_block", "%s SID %s blocking ID %s for override %s" % (self.__class__.__name__, sid, self.id, self.elec_block))
