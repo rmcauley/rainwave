766565ec3ddb3b9649335012827a6dc877d51421
Rob McAuley <rmcauley@rainwave.cc>
2014-02-11 03:05:58 +0900

    Fix group tag management

--- api_requests/admin/groups.py
+++ api_requests/admin/groups.py
@@ -1,33 +1,32 @@
-from libs import db
 import api.web
 from api.server import handle_api_url
-from api.server import handle_url
-from api.exceptions import APIException
 from api import fieldtypes
-from Rainwave.playlist import Song
+from rainwave.playlist import Song
 
-@handle_api_url("admin/add_group_cooldown")
-class AddGroupCooldown(api.web.APIHandler):
+@handle_api_url("admin/add_group_to_song")
+class AddGroupToSong(api.web.APIHandler):
 	admin_required = True
 	sid_required = False
-	description = "Add a group cooldown for a song"
+	description = "Add a group to a song."
 	fields = { "song_id": (fieldtypes.song_id, True),
-		"group": (fieldtypes.string, True)}
+		"group_name": (fieldtypes.string, True)}
 
 	def post(self):
 		s = Song.load_from_id(self.get_argument("song_id"));
 		#If comma-separated values, will do each individually
 		for group in self.get_argument("group").split(","):
 			s.add_group(group.strip())
+		self.append(self.return_name, { "success": "true", "text": "Group added to song." })
 			
-@handle_api_url("admin/remove_group_cooldown")
-class RemoveGroupCooldown(api.web.APIHandler):
+@handle_api_url("admin/remove_group_from_song")
+class RemoveGroupFromSong(api.web.APIHandler):
 	admin_required = True
 	sid_required = False
-	description = "Removes the group cooldown from a song."
+	description = "Removes the group from a song."
 	fields = { "song_id": (fieldtypes.song_id, True),
 		"group_id": (fieldtypes.group_id, True) }
 
 	def post(self):
 		s = Song.load_from_id(self.get_argument("song_id"));
 		s.remove_group_id(self.get_argument("group_id"))
+		self.append(self.return_name, { "success": "true", "tl_key": "Group removed from song ID." })
