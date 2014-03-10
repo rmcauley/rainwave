f8039e632bc2f3364df76e148e3a02874b567198
Rob McAuley <rmcauley@rainwave.cc>
2014-03-11 02:24:41 +0900

    Add request line admin page

---
+++ api_requests/admin/request_line.py
@@ -0,0 +1,23 @@
+from libs import cache
+from libs import db
+from libs import config
+import api.web
+from api.server import handle_api_url
+from api.server import handle_api_html_url
+from api.exceptions import APIException
+from api import fieldtypes
+from rainwave.events import event
+from rainwave.events.event import BaseProducer
+
+@handle_api_url("admin/request_line")
+class ListRequestLine(api.web.APIHandler):
+	return_name = "request_line"
+	admin_required = True
+	sid_required = True
+
+	def post(self):
+		self.append(self.return_name, cache.get_station(self.sid, "request_line"))
+
+@handle_api_html_url("request_line")
+class ListRequestLineHTML(api.web.PrettyPrintAPIMixin, ListRequestLine):
+	pass
