b139656f18c96040a3e9fe0e75fe6d1da8cac169
Rob McAuley <rmcauley@rainwave.cc>
2014-03-11 12:56:40 +0900

    Add request call results back

--- api_requests/request.py
+++ api_requests/request.py
@@ -25,6 +25,7 @@ class SubmitRequest(APIHandler):
 
 	def post(self):
 		if self.user.add_request(self.sid, self.get_argument("song_id")):
+			self.append_standard("request_success")
 			self.append("requests", self.user.get_requests(refresh=True))
 		else:
 			raise APIException("request_failed")
@@ -42,6 +43,7 @@ class DeleteRequest(APIHandler):
 
 	def post(self):
 		if self.user.remove_request(self.get_argument("song_id")):
+			self.append_standard("request_deleted")
 			self.append("requests", self.user.get_requests(refresh=True))
 		else:
 			raise APIException("request_delete_failed")
@@ -62,6 +64,7 @@ class OrderRequests(APIHandler):
 		for song_id in self.get_argument("order"):
 			db.c.update("UPDATE r4_request_store SET reqstor_order = %s WHERE user_id = %s AND song_id = %s", (order, self.user.id, song_id))
 			order = order + 1
+		self.append_standard("requests_reordered")
 		self.append("requests", self.user.get_requests(refresh=True))
 
 @handle_api_url("request_unrated_songs")
@@ -74,6 +77,7 @@ class RequestUnratedSongs(APIHandler):
 
 	def post(self):
 		if self.user.add_unrated_requests(self.sid) > 0:
+			self.append_standard("request_unrated_songs_success")
 			self.append("requests", self.user.get_requests(refresh=True))
 		else:
 			raise APIException("request_unrated_failed")
