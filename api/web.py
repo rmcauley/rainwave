0e0bf2fcb5544199d25edc7af381c19aa47c4f8f
MagnusVortex <magnusvortex@gmail.com>
2014-03-28 15:35:07 -0500

    Added in memory logging for each transaction call
    Also need to remove the current memory dump process so it doesn't interfere.

--- api/web.py
+++ api/web.py
@@ -146,6 +146,9 @@ class RainwaveHandler(tornado.web.RequestHandler):
 
 	# Called by Tornado, allows us to setup our request as we wish. User handling, form validation, etc. take place here.
 	def prepare(self):
+		#TODO: Shove this output into the trash.
+		objgraph.show_growth() #grab a snapshot of the data we have in place (to detect leaks)
+		
 		self._startclock = time.time()
 		self.user = None
 
@@ -347,6 +350,20 @@ class RainwaveHandler(tornado.web.RequestHandler):
 		if self.get_argument("page_start"):
 			limit += " OFFSET %s" % self.get_argument("page_start")
 		return limit
+		
+	def finish():
+		real_stdout = sys.stdout
+		temp_stdout = StringIO.StringIO()
+		sys.stdout = temp_stdout
+		if config.get("developer_mode") or config.test_mode:
+			log.debug("action", "API call happened")
+		objgraph.show_growth()
+		#I'm hoping the stuff in the {} of the next line is the name of the api call.
+		log.debug("memtrace", "\n {self.request.uri} API call happened. Leaks: \n" + sys.stdout.getvalue())
+		sys.stdout = real_stdout
+		temp_stdout.close()
+		super(RainwaveHandler, self).finish()
+
 
 class APIHandler(RainwaveHandler):
 	def initialize(self, **kwargs):
