de0d4703ade936ce97724fbdb31cda5dc435b8eb
Rob McAuley <rmcauley@gmail.com>
2014-07-20 02:16:42 +0900

    R4: Simplify album art code to use backgrounds

--- static/js4/albums.js
+++ static/js4/albums.js
@@ -3,57 +3,42 @@ var Albums = function() {
 	var self = {};
 
 	var expand_art = function(e) {
-		e.stopPropagation();
-		var tgt = e.target.nodeName == "IMG" ? e.target.parentNode : e.target;
-		if ($has_class(tgt, "art_expanded")) {
+		if ($has_class(e.target, "art_expanded")) {
 			normalize_art(e);
 			return;
 		}
 		
-		$add_class(tgt, "art_expanded");
+		$add_class(e.target, "art_expanded");
 		if (Mouse.x < (SCREEN_WIDTH - 270)) {
-			$add_class(tgt, "art_expand_right");
+			$add_class(e.target, "art_expand_right");
 		}
 		else {
-			$add_class(tgt, "art_expand_left");
+			$add_class(e.target, "art_expand_left");
 		}
 		if (Mouse.y < (SCREEN_HEIGHT - 270)) {
-			$add_class(tgt, "art_expand_down");
+			$add_class(e.target, "art_expand_down");
 		}
 		else {
-			$add_class(tgt, "art_expand_up");
+			$add_class(e.target, "art_expand_up");
 		}
 
-		if (("_album_art" in tgt) && tgt._album_art) {
-			var full_res = $el("img", { "class": "art_image", "src": tgt._album_art + ".jpg" });
+		if (("_album_art" in e.target) && e.target._album_art) {
+			var full_res = $el("img", { "class": "art_image", "src": e.target._album_art + ".jpg" });
 			full_res.onload = function() {
-				tgt.replaceChild(full_res, tgt.firstChild);
-				full_res.onload = null;	// enables the old image to be garbage collected
-				full_res.addEventListener("click", expand_art);
+				e.target.style.backgroundImage = "url(" + full_res.getAttribute("src") + ")";
 			};
-			tgt._album_art = null;
+			e.target._album_art = null;
 		}
-	};
-
-	var mouseout = function(e) {
-		var reltg = (e.relatedTarget) ? e.relatedTarget : e.toElement;    
-    	if ((reltg == e.target.parentNode) || (reltg == e.target.firstChild)) {
-    		e.stopPropagation();
-    		e.preventDefault();
-    		return false;
-    	}
-    	normalize_art(e);
 	};
 
 	var normalize_art = function(e) {
-		var tgt = e.target.nodeName == "IMG" ? e.target.parentNode : e.target;
-		tgt.style.zIndex = 2;
-		$remove_class(tgt, "art_expanded");
-		$remove_class(tgt, "art_expand_right");
-		$remove_class(tgt, "art_expand_left");
-		$remove_class(tgt, "art_expand_down");
-		$remove_class(tgt, "art_expand_up");
-		Fx.chain_transition(tgt, function() { tgt.style.zIndex = null; Fx.stop_chain(tgt); } );
+		e.target.style.zIndex = 2;
+		$remove_class(e.target, "art_expanded");
+		$remove_class(e.target, "art_expand_right");
+		$remove_class(e.target, "art_expand_left");
+		$remove_class(e.target, "art_expand_down");
+		$remove_class(e.target, "art_expand_up");
+		Fx.chain_transition(e.target, function() { e.target.style.zIndex = null; Fx.stop_chain(e.target); } );
 	};
 
 	self.art_html = function(json, size, not_expandable) {
@@ -62,19 +47,18 @@ var Albums = function() {
 		var img, ac;
 		if (!json.art) {
 			ac = c.appendChild($el("div", { "class": "art_container" }));
-			img = ac.appendChild($el("img", { "class": "art_image", "src": "/static/images4/noart_1.jpg" }));
+			ac.style.backgroundImage = "url(/static/images4/noart_1.jpg)";
 		}
 		else {
 			ac = c.appendChild($el("div", { "class": "art_container" }));
-			img = ac.appendChild($el("img", { "class": "art_image", "src": json.art + "_" + size + ".jpg" }));
+			ac.style.backgroundImage = "url(" + json.art + "_" + size + ".jpg)";
 			if (!not_expandable) {
 				$add_class(ac, "art_expandable")
-				ac._album_art = json.art || "/static/images4/test";
+				ac._album_art = json.art;
 				ac.addEventListener("click", expand_art);
-				img.addEventListener("click", expand_art);
-				ac.addEventListener("mouseout", mouseout);
+				ac.addEventListener("mouseout", normalize_art);
 			}
-		}
+		// }
 		return c;
 	};
 
