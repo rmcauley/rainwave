b800a092cc24766b901db1b74f02b7c35c08aa87
Rob McAuley <rmcauley@gmail.com>
2014-08-11 17:53:01 +0900

    Fix rating miscalculations due to poor inactive tracking

--- rainwave/playlist_objects/album.py
+++ rainwave/playlist_objects/album.py
@@ -246,9 +246,12 @@ class Album(AssociatedMetadata):
 			if not likes:
 				likes = 0
 			rating_count = dislikes + neutrals + neutralplus + likes
+			log.debug("song_rating", "%s album ratings for %s" % (rating_count, self.data['name']))
 			if rating_count > config.get("rating_threshold_for_calc"):
-				self.rating = round(((((likes + (neutrals * 0.5) + (neutralplus * 0.75)) / (likes + dislikes + neutrals + neutralplus) * 4.0)) + 1), 1)
-				db.c.update("UPDATE r4_album_sid SET album_rating = %s, album_rating_count = %s WHERE album_id = %s AND sid = %s", (self.rating, rating_count, self.id, sid))
+				self.data['rating'] = round(((((likes + (neutrals * 0.5) + (neutralplus * 0.75)) / (likes + dislikes + neutrals + neutralplus) * 4.0)) + 1), 1)
+				self.data['rating_count'] = rating_count
+				log.debug("song_rating", "%s new rating for %s" % (self.data['rating'], self.data['name']))
+				db.c.update("UPDATE r4_album_sid SET album_rating = %s, album_rating_count = %s WHERE album_id = %s AND sid = %s", (self.data['rating'], rating_count, self.id, sid))
 
 	def update_last_played(self, sid):
 		return db.c.update("UPDATE r4_album_sid SET album_played_last = %s WHERE album_id = %s AND sid = %s", (time.time(), self.id, sid))
