ff5ec5e57336d24a3c3673890c2d6fe2b9e6c908
Rob McAuley <rmcauley@gmail.com>
2014-08-16 02:25:34 +0900

    Revert "Flip ordering and lift time restriction for PHs"
    This reverts commit e8d9dfdb9da784cef1d103ffc75ab1211ae69c96.

--- api_requests/admin/power_hours.py
+++ api_requests/admin/power_hours.py
@@ -17,8 +17,8 @@ class ListPowerHours(api.web.APIHandler):
 		self.append(self.return_name,
 			db.c.fetch_all("SELECT sched_id AS id, sched_name AS name, sched_start AS start, sched_end AS end, sched_url AS url "
 						"FROM r4_schedule "
-						"WHERE sched_type = 'OneUpProducer' AND sched_used = FALSE AND sid = %s AND sched_start > %s ORDER BY sched_start DESC",
-						(self.sid, time.time() - (86400 * 26)))
+						"WHERE sched_type = 'OneUpProducer' AND sched_used = FALSE AND sid = %s AND sched_start > %s ORDER BY sched_start",
+						(self.sid, time.time() - 86400)))
 
 @handle_api_url("admin/get_power_hour")
 class GetPowerHour(api.web.APIHandler):
--- api_requests/admin/producers.py
+++ api_requests/admin/producers.py
@@ -1,4 +1,3 @@
-import time
 from libs import db
 import api.web
 from api.server import handle_api_url
@@ -17,8 +16,8 @@ class ListProducers(api.web.APIHandler):
 		self.append(self.return_name,
 			db.c.fetch_all("SELECT sched_type as type, sched_id AS id, sched_name AS name, sched_start AS start, sched_end AS end, sched_url AS url "
 						"FROM r4_schedule "
-						"WHERE sched_used = FALSE AND sid = %s AND sched_start > %s ORDER BY sched_start DESC",
-						(self.sid, time.time() - (86400 * 26))))
+						"WHERE sched_used = FALSE AND sid = %s ORDER BY sched_start",
+						(self.sid,)))
 
 @handle_api_url("admin/list_producer_types")
 class ListProducerTypes(api.web.APIHandler):
