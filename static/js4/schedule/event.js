d6aaca58ae21fed2e67c44828afe5742438c8907
Rob McAuley <robert.mcauley@coacha.com>
2014-09-08 18:58:19 +0900

    R4: Couple style fixes
    - Remove the aggressive hover styling on album links everywhere except
    voting-enabled timeline songs
    - Add "Vote Now" to timeline header when appropriate
    - Add hover state to Previously Played

--- static/js4/schedule/event.js
+++ static/js4/schedule/event.js
@@ -35,6 +35,8 @@ var EventBase = function(json) {
 	self.elements.header_clock = $el("span", { "class": "timeline_header_clock" });
 	var header_vote_result = $el("div", { "class": "timeline_header_vote_result" });
 	var header_text = $el("a");
+	var current_header_default_text;
+	var now_playing = false;
 	var header_bar = $el("div", { "class": "timeline_header_bar" });
 	var header_inside_bar = $el("div", { "class": "timeline_header_bar_inside" });
 	header_bar.appendChild(header_inside_bar);
@@ -92,13 +94,17 @@ var EventBase = function(json) {
 
 	self.change_to_coming_up = function() {
 		$add_class(self.el, "timeline_next");
-		self.set_header_text($l("coming_up"));
+		current_header_default_text = $l("coming_up");
+		now_playing = false;
+		self.set_header_text();
 	};
 
 	self.change_to_now_playing = function() {
 		if ($has_class(self.el, "timeline_now_playing")) return;
+		now_playing = true;
+		current_header_default_text = $l("now_playing");
 		$remove_class(self.el, "timeline_next");
-		self.set_header_text($l("now_playing"));
+		self.set_header_text();
 		Clock.pageclock = self.elements.header_clock;
 		if (self.songs && (self.songs.length > 1)) {
 			// other places in the code rely on songs[0] to be the winning song
@@ -132,6 +138,7 @@ var EventBase = function(json) {
 		for (var i = 0; i < self.songs.length; i++) {
 			self.songs[i].enable_voting();
 		}
+		self.set_header_text();
 	};
 
 	self.clear_voting_status = function() {
@@ -150,6 +157,7 @@ var EventBase = function(json) {
 		for (var i = 0; i < self.songs.length; i++) {
 			self.songs[i].disable_voting();
 		}
+		self.set_header_text();
 	};
 
 	self.register_vote = function(entry_id) {
@@ -168,21 +176,24 @@ var EventBase = function(json) {
 		Fx.delay_css_setting(self.el, "transform", "translateY(" + new_y + "px)");
 	};
 
-	self.set_header_text = function(default_text) {
+	self.set_header_text = function() {
 		if (self.type == "OneUp") {
-			header_text.textContent = default_text + " - " + self.name + " " + $l("power_hour");
+			header_text.textContent = current_header_default_text + " - " + self.name + " " + $l("power_hour");
 		}
-		else if ((default_text != $l("now_playing") || self.type != "Election") && $l_has(self.type.toLowerCase())) {
-			header_text.textContent = default_text + " - " + $l(self.type.toLowerCase());
+		else if (self.type != "Election" && $l_has(self.type.toLowerCase())) {
+			header_text.textContent = current_header_default_text + " - " + $l(self.type.toLowerCase());
 			if (self.name) {
 				header_text.textContent += " - " + self.name;
 			}
 		}
+		else if (!now_playing && self.type == "Election" && self.data.voting_allowed) {
+			header_text.textContent = current_header_default_text + " - " + $l("vote_now");
+		}
 		else if (self.name) {
-			header_text.textContent = default_text + " - " + self.name;
+			header_text.textContent = current_header_default_text + " - " + self.name;
 		}
 		else {
-			header_text.textContent = default_text;
+			header_text.textContent = current_header_default_text;
 		}
 		if (self.data.url) {
 			header_text.setAttribute("href", self.data.url);
