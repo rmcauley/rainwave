99de0d3834e025c920559966c1cfd72116de66e9
Rob McAuley <robert.mcauley@coacha.com>
2014-09-22 15:21:06 +0900

    Bring Blorp's variable name up to standard
    Also tweak his SQL a bit to give Postgres some optimization hints

--- api_requests/playlist.py
+++ api_requests/playlist.py
@@ -171,9 +171,12 @@ class AllFavHandler(APIHandler):
 	def post(self):
 		if 'sid' in self.request.arguments:
 			self.append(self.return_name, db.c.fetch_all(
-			"SELECT r4_song_ratings.song_id AS id, song_title AS title, r4_albums.album_id, album_name, song_rating AS rating, COALESCE(song_rating_user, 0) AS rating_user, song_fave AS fave, r4_song_sid.song_cool_end AS end_cooldown "
-			"FROM r4_song_ratings JOIN r4_songs USING (song_id) JOIN r4_albums USING (album_id) JOIN r4_song_sid USING (song_id)"
-			"WHERE user_id = %s AND song_verified = TRUE AND song_fave = TRUE AND r4_song_sid.sid = %s ORDER BY album_name, song_title " + self.get_sql_limit_string(), (self.user.id, self.sid,)))
+			"SELECT r4_song_ratings.song_id AS id, song_title AS title, r4_albums.album_id, album_name, song_rating AS rating, COALESCE(song_rating_user, 0) AS rating_user, song_fave AS fave, r4_song_sid.song_cool_end AS cool_end "
+			"FROM r4_song_ratings "
+				"JOIN r4_song_sid ON (r4_song_ratings.song_id = r4_song_sid.song_id AND r4_song_sid.sid = %s) "
+				"JOIN r4_songs ON (r4_song_ratings.song_id = r4_songs.song_id) "
+				"JOIN r4_albums USING (album_id) "
+			"WHERE user_id = %s AND song_verified = TRUE AND song_fave = TRUE ORDER BY album_name, song_title " + self.get_sql_limit_string(), (self.sid, self.user.id)))
 		else:
 			self.append(self.return_name, db.c.fetch_all(
 			"SELECT r4_song_ratings.song_id AS id, song_title AS title, r4_albums.album_id, album_name, song_rating AS rating, COALESCE(song_rating_user, 0) AS rating_user, song_fave AS fave "
