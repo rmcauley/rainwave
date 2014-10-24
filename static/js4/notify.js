c3801ff19799f89ec328b3c7f995963a467173c4
Rob McAuley <rmcauley@gmail.com>
2014-10-24 20:59:07 +0900

    Repeated notify fix, disable notify on mobile

--- static/js4/notify.js
+++ static/js4/notify.js
@@ -4,10 +4,12 @@ var R4Notify = function() {
 	var self = {};
 	self.capable = Notification ? true : false;
 	self.enabled = false;
+	var current_song_id;
 
 	self.initialize = function() {
 		Prefs.define("notify", [ false, true ]);
 		if (!self.capable) return;
+		if (MOBILE) return;
 
 		Prefs.add_callback("notify", self.check_permission);
 		self.check_permission();
@@ -37,6 +39,8 @@ var R4Notify = function() {
 		if (!Prefs.get("notify")) return;
 		if (!sched_current || !sched_current.songs || sched_current.songs.length == 0) return;
 		if ($has_class(document.body, "loading")) return;
+		if (sched_current.songs[0].id == current_song_id) return;
+		current_song_id = sched_current.songs[0].id;
 
 		var art = sched_current.songs[0].albums[0].art ? sched_current.songs[0].albums[0].art : "/static/images4/noart_1.jpg";
 		var artists = "";
