9df083b1c4811705e85c90d1040b9d021676bdf5
Rob McAuley <rmcauley@gmail.com>
2014-10-26 18:10:17 +0900

    Fix using invalid testing map in auto_pvp

--- rw_auto_pvp.py
+++ rw_auto_pvp.py
@@ -22,13 +22,13 @@ if __name__ == "__main__":
 	db.connect()
 	cache.connect()
 	
-	# dow_map = [ (4, 5), (2, 1), (3, 4), (5, 2), (1, 3), (2, 4), (1, 3) ]
-	dow_map = [ (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1) ]
+	dow_map = [ (4, 5), (2, 1), (3, 4), (5, 2), (1, 3), (2, 4), (1, 3) ]
+	# dow_map = [ (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1) ]
 	timezones = [ (timezone('Europe/London'), 13, 0), (timezone('US/Eastern'), 13, 1) ]
 
 	for tz in timezones:
 		start = datetime.now(tz[0]).replace(hour=tz[1], minute=0, second=0, microsecond=0)
 		sid = dow_map[start.weekday()][tz[2]]
-		start_e = (start - datetime.fromtimestamp(0, tz[0])).total_seconds()
+		start_e = (start - datetime.fromtimestamp(0, timezone('US/Eastern'))).total_seconds()
 		log.debug("auto_pvp", "%04d/%02d/%02d %02d:%02d PVP %s %s" % (start.year, start.month, start.day, start.hour, start.minute, config.station_id_friendly[sid], tz[0].__class__.__name__))
 		pvpelection.PVPElectionProducer.create(sid, start_e, start_e + 3600, name="PvP Hour")
