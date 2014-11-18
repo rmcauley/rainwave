42ca486a0a40cd051198c830e43ac9103429fff6
Rob McAuley <robert.mcauley@coacha.com>
2014-11-18 19:49:19 +0900

    Fix function call

--- rainwave/user.py
+++ rainwave/user.py
@@ -419,7 +419,7 @@ class User(object):
 		if self.id == 1:
 			api_key = db.c.fetch_var("SELECT api_key FROM r4_api_keys WHERE user_id = 1 AND api_ip = %s", (self.ip_address,))
 			if not api_key:
-				api_key = self.generate_api_key(self.ip_address, int(time.time()) + 172800)
+				api_key = self.generate_api_key(int(time.time()) + 172800)
 				cache.set("ip_%s_api_key" % self.ip_address, api_key)
 		elif self.id > 1:
 			if 'api_key' in self.data and self.data['api_key']:
