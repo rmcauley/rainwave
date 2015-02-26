2323a68da3df6d290e0bd2befed97db0fe3fb7e9
Rob McAuley <rmcauley@gmail.com>
2015-02-26 22:31:21 +0900

    Add liquidsoap library for testing

---
+++ api/liquidsoap.py
@@ -0,0 +1,33 @@
+import socket
+import os, os.path
+
+from api.exceptions import APIException
+from libs import config
+
+def _send_command(sid, cmd):
+	cmd += "\n"
+	socket_path = config.get_station(sid, "liquidsoap_socket_path")
+	if os.path.exists(socket_path):  
+		client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+		client.connect(socket_path)
+		client.send(cmd)
+		to_ret = client.recv(1024)
+		client.close()
+		return to_ret
+	else:
+		raise APIException("internal_error", "Could not connect to Liquidsoap.")
+
+def pause(sid):
+	return _send_command(sid, "var.set paused = true")
+
+def unpause(sid):
+	return _send_command(sid, "var.set paused = false")
+
+def skip(sid):
+	return _send_command(sid, "%s(dot)mp3.skip" % config.get_station(sid, "stream_filename"))
+ 
+def set_password(sid, password):
+	return _send_command(sid, "var.set harbor_pw = \"%s\"" % password)
+
+def kick_dj(sid):
+	return _send_command(sid, "harbor.kick")
