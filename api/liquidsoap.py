8f53f8a881848ec3d3371792a2a974e435ee42de
Rob McAuley <robert.mcauley@coacha.com>
2015-03-12 14:04:23 +0900

    Tested and testable liquidsoap

--- api/liquidsoap.py
+++ api/liquidsoap.py
@@ -7,15 +7,16 @@ from libs import config
 def _send_command(sid, cmd):
 	cmd += "\n"
 	socket_path = config.get_station(sid, "liquidsoap_socket_path")
-	if os.path.exists(socket_path):  
-		client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+	if os.path.exists(socket_path):
+		client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		client.connect(socket_path)
 		client.send(cmd)
 		to_ret = client.recv(1024)
 		client.close()
+		to_ret = to_ret.strip().strip("END").strip()
 		return to_ret
 	else:
-		raise APIException("internal_error", "Could not connect to Liquidsoap.")
+		raise APIException("internal_error", "Could not connect to Liquidsoap. (station %s)" % sid)
 
 def pause(sid):
 	return _send_command(sid, "var.set paused = true")
@@ -25,9 +26,12 @@ def unpause(sid):
 
 def skip(sid):
 	return _send_command(sid, "%s(dot)mp3.skip" % config.get_station(sid, "stream_filename"))
- 
+
 def set_password(sid, password):
 	return _send_command(sid, "var.set harbor_pw = \"%s\"" % password)
 
 def kick_dj(sid):
 	return _send_command(sid, "harbor.kick")
+
+def test(sid):
+	return _send_command(sid, "version")
