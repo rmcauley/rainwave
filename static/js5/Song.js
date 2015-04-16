9ad87a2770cfb2c9684d8e8f98ec7657c1c8b42a
Rob McAuley <robert.mcauley@coacha.com>
2015-04-16 16:58:10 +0900

    Tie ratings into song

--- static/js5/Song.js
+++ static/js5/Song.js
@@ -3,6 +3,13 @@ var TimelineSong = function(self, event) {
 	self.$template = RWTemplates.song(self);
 	self.el = self.$template.root;
 
+	if (self.$template.rating_el) {
+		Rating.register(self);
+	}
+	if (self.albums[0].$template.rating_el) {
+		Rating.register(self.albums[0]);
+	}
+
 	var voting_enabled = false;
 	self.vote = function(evt) {
 		if (!voting_enabled) {
@@ -21,11 +28,23 @@ var TimelineSong = function(self, event) {
 
 		self.$template.votes.textContent = self.entry_votes;
 
-		if (self.rating && self.rating.update) {
-		 	self.rating.update(self);
+		if (self.$template.rating_el) {
+			if (self.rating_user) {
+				self.$template.rating_el.classList.add("rating_user");
+			}
+			else {
+				self.$template.rating_el.classList.remove("rating_user");
+			}
+		 	self.$template.rating_el.rating_set(self.rating_user || self.rating);
 		}
-		if (self.albums[0].album_rating && self.albums[0].album_rating.update) {
-		 	self.albums[0].rating.update(self.albums[0]);
+		if (self.albums[0].$template.rating_el) {
+			if (self.rating_user) {
+				self.albums[0].$template.rating_el.classList.add("rating_user");
+			}
+			else {
+				self.albums[0].$template.rating_el.classList.remove("rating_user");
+			}
+		 	self.albums[0].$template_el.rating_set(self.rating_user || self.rating);
 		}
 
 		self.update_cooldown_info();
@@ -45,12 +64,12 @@ var TimelineSong = function(self, event) {
 		}
 		else if (self.cool) {
 			self.el.classList.add("timeline_song_is_cool");
-			self.elements.cooldown.textContent = $l("request_on_cooldown_ends_soon");
+			self.$template.cooldown.textContent = $l("request_on_cooldown_ends_soon");
 		}
 		else if (self.elec_blocked) {
 			self.el.classList.add("timeline_song_is_cool");
 			self.elec_blocked_by = self.elec_blocked_by.charAt(0).toUpperCase() + self.elec_blocked_by.slice(1);
-			self.elements.cooldown.textContent = $l("request_in_election", { "blocked_by": $l("blocked_by_name__" + self.elec_blocked_by.toLowerCase()) });
+			self.$template.cooldown.textContent = $l("request_in_election", { "blocked_by": $l("blocked_by_name__" + self.elec_blocked_by.toLowerCase()) });
 		}
 		else {
 			self.el.classList.remove("timeline_song_is_cool");
@@ -79,14 +98,6 @@ var TimelineSong = function(self, event) {
 
 	self.unregister_vote = function() {
 		self.el.classList.remove("voting_registered");
-	};
-
-	self.rate = function(new_rating) {
-		self.rating.rate(new_rating);
-	};
-
-	self.destroy = function() {
-		// clean up ratings
 	};
 
 	return self;
