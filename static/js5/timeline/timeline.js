3fb132536d33f3b0f7123d876040920ed7302ebc
Rob McAuley <rmcauley@gmail.com>
2015-11-08 18:55:14 +0900

    Add station lock and event messages

--- static/js5/timeline/timeline.js
+++ static/js5/timeline/timeline.js
@@ -25,6 +25,8 @@ var Timeline = function() {
 		API.add_callback("user", self.voting_allowed_check);
 		API.add_callback("playback_history", open_long_history);
 		API.add_callback("already_voted", self.handle_already_voted);
+		API.add_callback("all_stations_info", check_for_events);
+		API.add_callback("user", lock_check);
 
 		template = RWTemplates.timeline.timeline();
 		root_template.timeline.parentNode.replaceChild(template.timeline, root_template.timeline);
@@ -168,7 +170,7 @@ var Timeline = function() {
 		}
 	};
 
-	self.add_message = function(id, text, not_closeable) {
+	self.add_message = function(id, text, not_closeable, no_reflow) {
 		for (var i = 0; i < messages.length; i++) {
 			if (messages[i].id == id) {
 				return;
@@ -187,10 +189,12 @@ var Timeline = function() {
 		messages.push(msg);
 		if (msg.$t.close) {
 			msg.$t.close.addEventListener("click", function() {
-				self.close_message(id);
+				self.remove_message(id);
 			});
 		}
-		self.reflow();
+		if (!no_reflow) {
+			self.reflow();
+		}
 		return msg;
 	};
 
@@ -385,6 +389,54 @@ var Timeline = function() {
 		// w.appendChild(t);
 
 		// Menu.show_modal($id("longhist_window_container"));
+	};
+
+	var do_event = function(json, sid) {
+		var url;
+		for (var i = 0; i < Stations.length; i++) {
+			if (Stations[i].id == sid) {
+				url = Stations[i].url;
+			}
+		}
+		var msg = self.add_message("event_" + sid, $l("special_event_on_now"), false, true);
+		var xmsg = document.createElement("span");
+		xmsg.textContent = Formatting.event_name(json.event_type, json.event_name);
+		msg.$t.message.appendChild(xmsg);
+		msg.$t.el.addEventListener("click", function() {
+			window.location.href = url;
+		});
+		msg.$t.el.style.cursor = "pointer";
+		return msg;
+	};
+
+	var check_for_events = function(json) {
+		var sid;
+		for (sid in json) {
+			if (json[sid] && json[sid].event_name && (sid != User.sid)) {
+				do_event(json[sid]);
+			}
+			else if (!json[sid].event_name) {
+				self.close_message("event_" + sid);
+			}
+		}
+	};
+
+	var lock_check = function(json) {
+		if (json.lock_in_effect && (json.lock_sid != User.sid)) {
+			var locked_name, this_name;
+			for (var i = 0; i < Stations.length; i++) {
+				if (Stations[i].id == json.lock_sid) {
+					locked_name = Stations[i].name;
+				}
+				else if (Stations[i].sid == User.sid) {
+					this_name = Stations[i].name;
+				}
+			}
+			self.add_message("station_lock", $l("locked_to_station", { "locked_station": locked_name, "this_station": this_name, "lock_counter": json.lock_counter }), true, true);
+		}
+		else {
+			self.remove_message("station_lock");
+		}
 	};
 
 	return self;
