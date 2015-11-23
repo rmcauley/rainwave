940be2d4e82a1a41fd5e5da656eefc184063a109
Rob McAuley <rmcauley@gmail.com>
2015-11-23 13:55:26 +0900

    Stat dump script fixes

--- rw_stat_dump.py
+++ rw_stat_dump.py
@@ -7,10 +7,34 @@ import sys
 import libs.db
 import rainwave.playlist
 
-parser = argparse.ArgumentParser(description="Dumps data to /tmp/rwstats in CSV format from SQL")
+parser = argparse.ArgumentParser(description="Dumps data to /tmp/rwstats in CSV format from SQL. Dangerous script in many, many ways - needs Postgres superuser permissions.")
 parser.add_argument("--config", default=None)
 parser.add_argument("--sid", type=int)
 args = parser.parse_args()
+
+# Before running this script you're going to need pseudo_encrypt in the database:
+'''
+CREATE OR REPLACE FUNCTION pseudo_encrypt(VALUE int) returns int AS $$
+DECLARE
+l1 int;
+l2 int;
+r1 int;
+r2 int;
+i int:=0;
+BEGIN
+ l1:= (VALUE >> 16) & 65535;
+ r1:= VALUE & 65535;
+ WHILE i < 3 LOOP
+   l2 := r1;
+   r2 := l1 # ((((1366 * r1 + 150889) % 714025) / 714025.0) * 32767)::int;
+   l1 := l2;
+   r1 := r2;
+   i := i + 1;
+ END LOOP;
+ RETURN ((r1 << 16) + l1);
+END;
+$$ LANGUAGE plpgsql strict immutable;
+'''
 
 libs.config.load(args.config)
 libs.db.connect()
@@ -38,19 +62,20 @@ tables = [
 	'r4_vote_history'
 ]
 
-os.mkdir("/tmp/rwstats")
-
-for table in tables:
-	print table
-	sys.stdout.flush()
-	query = libs.db.c.fetch_var(
-		"SELECT "
-			"'SELECT ' || "
-				"ARRAY_TO_STRING(ARRAY(SELECT CASE WHEN COLUMN_NAME::VARCHAR(50)='user_id' THEN 'pseudo_encrypt(user_id) AS user_id' ELSE COLUMN_NAME::VARCHAR(50) END  "
-    		"FROM INFORMATION_SCHEMA.COLUMNS "
-    		"WHERE TABLE_NAME='%s' AND "
-            "COLUMN_NAME NOT IN ('col2') "
-    		"ORDER BY ORDINAL_POSITION "
-		"), ', ') || ' FROM %s'" % (table, table)
-	)
-	libs.db.c.update("COPY (%s) TO '/tmp/rwstats/%s.csv' WITH CSV" % (table, table))
+if not os.path.exists("/tmp/rwstats"):
+	print "Make sure /tmp/rwstats exists and is world writable."
+else:
+	for table in tables:
+		print table
+		sys.stdout.flush()
+		query = libs.db.c.fetch_var(
+			"SELECT "
+				"'SELECT ' || "
+					"ARRAY_TO_STRING(ARRAY(SELECT CASE WHEN COLUMN_NAME::VARCHAR(50)='user_id' THEN 'pseudo_encrypt(user_id) AS user_id' ELSE COLUMN_NAME::VARCHAR(50) END  "
+	    		"FROM INFORMATION_SCHEMA.COLUMNS "
+	    		"WHERE TABLE_NAME='%s' AND "
+	            "COLUMN_NAME NOT IN ('col2') "
+	    		"ORDER BY ORDINAL_POSITION "
+			"), ', ') || ' FROM %s'" % (table, table)
+		)
+		libs.db.c.update("COPY (%s) TO '/tmp/rwstats/%s.csv' WITH CSV HEADER" % (query, table))
