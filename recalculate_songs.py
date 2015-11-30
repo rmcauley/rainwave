1a39c49e940e8a04f7de19f2a06453d774a3da9a
Rob McAuley <robert.mcauley@coacha.com>
2015-11-30 14:41:43 +0900

    Add recalculate song script

---
+++ recalculate_songs.py
@@ -0,0 +1,23 @@
+import argparse
+
+from libs import config
+from libs import db
+from rainwave.playlist import Song
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Recalculates all song global ratings.  Can take a while.")
+	parser.add_argument("--config", default=None)
+	args = parser.parse_args()
+	config.load(args.config)
+	db.connect()
+
+	songs = db.c.fetch_list("SELECT song_id FROM r4_songs")
+	i = 0
+	for song_id in songs:
+		txt = "Song %s / %s" % (i, len(songs))
+		txt += " " * (80 - len(txt))
+		print "\r" + txt,
+		i += 1
+
+		s = Song.load_from_id(song_id, True)
+		s.update_rating()
