8d31a1ab6340c50b7df930704da0428176167a69
Rob McAuley <rmcauley@gmail.com>
2016-01-04 19:41:29 +0900

    More logo sizes for Android

--- api_requests/manifest.py
+++ api_requests/manifest.py
@@ -64,6 +64,16 @@ class ManifestJSON(HTMLRequest):
 					"src": "/static/images4/android/256.png",
 					"sizes": "256x256",
 					"type": "image/png"
+				},
+				{
+					"src": "/static/images4/android/384.png",
+					"sizes": "384x384",
+					"type": "image/png"
+				},
+				{
+					"src": "/static/images4/android/512.png",
+					"sizes": "512x512",
+					"type": "image/png"
 				}
 			]
 		}
