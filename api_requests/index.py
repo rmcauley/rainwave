5ecac6d7147328fd21349e4d4a28f8d29bf4802a
Rob McAuley <rmcauley@gmail.com>
2016-02-28 19:39:39 +0900

    Remove apple_home_screen icon

--- api_requests/index.py
+++ api_requests/index.py
@@ -83,7 +83,6 @@ class MainIndex(api.web.HTMLRequest):
 			# relays=config.public_relays_json[self.sid],
 			# stream_filename=config.get_station(self.sid, "stream_filename"),
 			# station_list=config.station_list_json,
-			apple_home_screen_icon=config.get_station(self.sid, "apple_home_screen_icon"),
 			mobile=self.mobile,
 			station_name=page_title,
 			dj=self.user.is_dj()
