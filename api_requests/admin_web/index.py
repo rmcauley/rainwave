799f483888076a14d6b962eb966ee3e37bfcfa8f
Rob McAuley <rmcauley@gmail.com>
2016-03-03 13:24:02 +0900

    Admin panel album list also supports sorting by added_on

--- api_requests/admin_web/index.py
+++ api_requests/admin_web/index.py
@@ -148,19 +148,23 @@ class AlbumList(api.web.HTMLRequest):
 	admin_required = True
 	allow_get = True
 	allow_sid_zero = True
-	fields = { "restrict": (fieldtypes.sid, True) }
+	fields = { "restrict": (fieldtypes.sid, True), "sort": (fieldtypes.string, None) }
 
 	def get(self):
 		self.write(self.render_string("bare_header.html", title="Album List"))
 		self.write("<h2>%s Playlist</h2>" % config.station_id_friendly[self.get_argument('restrict')])
 		self.write("<table>")
-		albums = db.c.fetch_all("SELECT r4_albums.album_id AS id, album_name AS name, album_name_searchable AS name_searchable, album_rating AS rating, album_cool AS cool, album_cool_lowest AS cool_lowest, album_updated AS updated, album_fave AS fave, album_rating_user AS rating_user, album_cool_multiply AS cool_multiply, album_cool_override AS cool_override "
+		sql = ("SELECT r4_albums.album_id AS id, album_name AS name, album_name_searchable AS name_searchable, album_rating AS rating, album_cool AS cool, album_cool_lowest AS cool_lowest, album_updated AS updated, album_fave AS fave, album_rating_user AS rating_user, album_cool_multiply AS cool_multiply, album_cool_override AS cool_override "
 			"FROM r4_albums "
 			"JOIN r4_album_sid USING (album_id) "
 			"LEFT JOIN r4_album_ratings ON (r4_album_sid.album_id = r4_album_ratings.album_id AND user_id = %s AND r4_album_ratings.sid = r4_album_sid.sid) "
 			"WHERE r4_album_sid.sid = %s AND r4_album_sid.album_exists = TRUE "
-			"ORDER BY album_name",
-			(self.user.id, self.get_argument("restrict")))
+		)
+		if self.get_argument("sort", None) and self.get_argument("sort") == "added_on":
+			sql += "ORDER BY album_newest_song_time DESC, album_name"
+		else:
+			sql += "ORDER BY album_name"
+		albums = db.c.fetch_all(sql, (self.user.id, self.get_argument("restrict")))
 		for row in albums:
 			self.write("<tr><td>%s</td>" % row['id'])
 			self.write("<td onclick=\"window.location.href = '../song_list/' + window.top.current_tool + '?sid=%s&id=%s&sort=%s';\" style='cursor: pointer;'>%s</td><td>" % (self.get_argument('restrict'), row['id'], self.get_argument('sort', ""), row['name']))
