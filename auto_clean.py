86df6082bd52f8c3dd1c5c5f01b8beba7122a064
Rob McAuley <rmcauley@gmail.com>
2016-06-29 10:11:43 +0900

    Fix invalid argument in auto_clean

--- auto_clean.py
+++ auto_clean.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 	for row in remove_songs:
 		fn = row['song_filename'].split(os.sep)[-1]
 		mkdir_p("%s%s%s" % (args.moveto, os.sep, row['song_origin_sid']))
-		shutil.move(row, "%s%s%s%s%s" % (args.moveto, os.sep, row['song_origin_sid'], os.sep, row['song_filename']))
+		shutil.move(row['song_filename'], "%s%s%s%s%s" % (args.moveto, os.sep, row['song_origin_sid'], os.sep, row['song_filename']))
 
 		song = Song.load_from_id(row['song_id'])
 		song.disable()
