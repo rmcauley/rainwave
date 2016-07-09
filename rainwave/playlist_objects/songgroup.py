ab3c5f0657846ac59632e868cd6f6d9f03d33181
Rob McAuley <rmcauley@gmail.com>
2016-07-09 10:40:21 +0900

    Fix song group ordering

--- rainwave/playlist_objects/songgroup.py
+++ rainwave/playlist_objects/songgroup.py
@@ -30,6 +30,7 @@ class SongGroup(AssociatedMetadata):
 				"JOIN r4_group_sid ON (r4_song_group.group_id = r4_group_sid.group_id AND r4_group_sid.sid = %s AND r4_group_sid.group_display = TRUE) "
 				"JOIN r4_groups ON (r4_group_sid.group_id = r4_groups.group_id) "
 			"WHERE r4_song_sid.song_id = %s AND r4_song_sid.sid = %s AND song_exists = TRUE "
+			"ORDER BY r4_groups.group_name"
 			,(sid, song_id, sid)
 		)
 		instances = []
