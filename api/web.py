4e44d6738350dc1845a851241f6acbe4a4b4b861
Rob McAuley <rmcauley@gmail.com>
2017-09-23 22:23:59 +0900

    Revert secure cookies, don't know how to use them

--- api/web.py
+++ api/web.py
@@ -154,7 +154,7 @@ class RainwaveHandler(tornado.web.RequestHandler):
 	def set_cookie(self, name, value, *args, **kwargs):
 		if isinstance(value, (int, long)):
 			value = repr(value)
-		super(RainwaveHandler, self).set_cookie(name, "{}; Secure".format(value), *args, **kwargs)
+		super(RainwaveHandler, self).set_cookie(name, value, *args, **kwargs)
 
 	def get_argument(self, name, default=None, **kwargs):
 		if name in self.cleaned_args:
