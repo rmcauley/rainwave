0d8e41eb583cf982b914e642c6dfd68da5a65548
William Jackson <william@subtlecoolness.com>
2019-03-20 23:32:38 -0500

    Fix songs not appearing in unrated songs list
    If a song has a rating and the user clears the rating, that song will not appear in the list of unrated songs.

--- rainwave/playlist.py
+++ rainwave/playlist.py
@@ -246,7 +246,7 @@ def get_unrated_songs_for_user(user_id, limit = "LIMIT ALL"):
 		"SELECT r4_songs.song_id AS id, song_title AS title, album_name "
 		"FROM r4_songs JOIN r4_albums USING (album_id) "
 		"LEFT OUTER JOIN r4_song_ratings ON (r4_songs.song_id = r4_song_ratings.song_id AND user_id = %s) "
-		"WHERE song_verified = TRUE AND r4_song_ratings.song_id IS NULL AND song_origin_sid > 0 ORDER BY album_name, song_title " + limit, (user_id,))
+		"WHERE song_verified = TRUE AND song_rating_user IS NULL AND song_origin_sid > 0 ORDER BY album_name, song_title " + limit, (user_id,))
 
 def _get_requested_albums_sql():
 	if not db.c.allows_join_on_update:
