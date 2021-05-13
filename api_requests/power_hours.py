1b6ab72bae2f9004cedd146e83e4a40a5a6f3335
Rob McAuley <rmcauley@gmail.com>
2021-05-13 19:16:30 +0200

    Fix PH list to only display future PHs

--- api_requests/power_hours.py
+++ api_requests/power_hours.py
@@ -1,14 +1,28 @@
+from time import time as timestamp
+import api
+
 from api.urls import handle_api_url
 from api.urls import handle_api_html_url
 from api.web import PrettyPrintAPIMixin
+from libs import db
 
-from api_requests.admin.power_hours import ListPowerHours as ListPowerHoursAdmin
 from api_requests.admin_web.power_hours import get_ph_formatted_time
 
 
 @handle_api_url("power_hours")
-class ListPowerHours(ListPowerHoursAdmin):
+class ListPowerHours(api.web.APIHandler):
     admin_required = False
+
+    def post(self):
+        self.append(
+            self.return_name,
+            db.c.fetch_all(
+                "SELECT sched_id AS id, sched_name AS name, sched_start AS start, sched_end AS end, sched_url AS url "
+                "FROM r4_schedule "
+                "WHERE sched_type = 'OneUpProducer' AND sid = %s AND sched_start > %s ORDER BY sched_start ASC",
+                (self.sid, timestamp()),
+            ),
+        )
 
 SHOW_TIMEZONES = ["US/Pacific", "US/Eastern", "Europe/London", "Europe/Berlin", "Asia/Tokyo"]
 
