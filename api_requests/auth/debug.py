9f126f919e3abc5af3b59ad53cdc6319ea5849ad
Rob McAuley <rmcauley@gmail.com>
2021-05-14 20:20:52 +0200

    Hopefully fix Discord first time login

--- api_requests/auth/debug.py
+++ api_requests/auth/debug.py
@@ -18,13 +18,15 @@ class DiscordAuth(HTMLRequest):
         has_phpbb_auth = self.do_phpbb_auth()
         has_session_auth = self.do_rw_session_auth()
         discord_id = db.c.fetch_var("SELECT discord_user_id FROM phpbb_users WHERE user_id = %s", (self.user.id,))
+        username = db.c.fetch_var("SELECT username FROM phpbb_users WHERE user_id = %s", (self.user.id,))
         radio_username = db.c.fetch_var("SELECT radio_username FROM phpbb_users WHERE user_id = %s", (self.user.id,))
 
-        self.write(f"phpBB Auth? {has_phpbb_auth}<br />")
-        self.write(f"OAuth? {has_session_auth}<br />")
-        self.write(f"Discord? {discord_id}<br />")
-        self.write(f"phpBB Username? {self.user.data['name']}<br />")
-        self.write(f"Display Username? {radio_username}<br />")
+        self.write(f"User ID: {self.user.id}<br />")
+        self.write(f"phpBB Auth: {has_phpbb_auth}<br />")
+        self.write(f"RW Session Auth: {has_session_auth}<br />")
+        self.write(f"Discord ID: {discord_id}<br />")
+        self.write(f"phpBB Username: {username}<br />")
+        self.write(f"Display Username: {radio_username}<br />")
 
         self.write(self.render_string("basic_footer.html"))
 
