326265cfdf3199676f3d6bd53c2e9a89b8469a3d
Rob McAuley <rmcauley@gmail.com>
2021-05-28 22:45:04 +0200

    Add more wormgas commands

--- api_requests/admin/enable_perks.py
+++ api_requests/admin/enable_perks.py
@@ -5,6 +5,8 @@ from api import fieldtypes
 from api.exceptions import APIException
 from libs import config
 
+PRIVILEGED_GROUP_IDS = (18, 5, 4)
+
 @handle_api_url("enable_perks_by_discord_ids")
 class UserSearchByDiscordUserIdRequest(api.web.APIHandler):
     auth_required = False
@@ -17,12 +19,8 @@ class UserSearchByDiscordUserIdRequest(api.web.APIHandler):
         if self.request.remote_ip not in config.get("api_trusted_ip_addresses"):
             raise APIException("auth_failed", f"{self.request.remote_ip} is not allowed to access this endpoint.")
 
-        for discord_user_id in self.get_argument("discord_user_ids"):
-          possible_id = db.c.fetch_var(
-              "SELECT user_id FROM phpbb_users WHERE discord_user_id = %s",
-              (discord_user_id,),
-          )
-          if possible_id:
-              db.c.update("UPDATE phpbb_users SET group_id = 5 WHERE user_id = %s", (possible_id,))
+        list_as_tuple = tuple(self.get_argument("discord_user_ids"))
+
+        db.c.update("UPDATE phpbb_users SET group_id = 8 WHERE user_id IN %s AND group_id NOT IN %s AND group_id != 8", (list_as_tuple, PRIVILEGED_GROUP_IDS))
 
         self.append_standard("yes")
