2215302ba8fc1e6ce2658ff5889dd5b814e62fc5
Rob McAuley <rmcauley@gmail.com>
2021-07-28 22:07:51 +0200

    Make power_hours page all-station

--- api_requests/power_hours.py
+++ api_requests/power_hours.py
@@ -5,6 +5,7 @@ from api.urls import handle_api_url
 from api.urls import handle_api_html_url
 from api.web import PrettyPrintAPIMixin
 from libs import db
+from libs import config
 
 from api_requests.admin_web.power_hours import get_ph_formatted_time
 
@@ -13,15 +14,16 @@ from api_requests.admin_web.power_hours import get_ph_formatted_time
 class ListPowerHours(api.web.APIHandler):
     return_name = "power_hours"
     admin_required = False
+    sid_required = False
 
     def post(self):
         self.append(
             self.return_name,
             db.c.fetch_all(
-                "SELECT sched_id AS id, sched_name AS name, sched_start AS start, sched_end AS end, sched_url AS url "
+                "SELECT sid, sched_id AS id, sched_name AS name, sched_start AS start, sched_end AS end, sched_url AS url "
                 "FROM r4_schedule "
-                "WHERE sched_type = 'OneUpProducer' AND sid = %s AND sched_start > %s ORDER BY sched_start ASC",
-                (self.sid, timestamp()),
+                "WHERE sched_type = 'OneUpProducer' AND sched_start > %s ORDER BY sched_start ASC",
+                (timestamp(),),
             ),
         )
 
@@ -30,9 +32,12 @@ SHOW_TIMEZONES = ["US/Pacific", "US/Eastern", "Europe/London", "Europe/Berlin", 
 @handle_api_html_url("power_hours")
 class AllRequestedSongsHTML(PrettyPrintAPIMixin, ListPowerHours):
     def header_special(self):
+        self.write("<th>Station</th>")
         self.write("<th>Date and Time</th>")
 
     def row_special(self, row):
+        station_friendly = config.station_id_friendly[row['sid']],
+        self.write(f"<td>{station_friendly}</td>")
         self.write("<td><ul>")
         for tz in SHOW_TIMEZONES:
           self.write(
