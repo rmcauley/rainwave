5c63c83b2cebec89bc2e7019945be18a8f8b77f5
Rob McAuley <rmcauley@gmail.com>
2024-12-28 16:02:34 +0100

    Use super() instead of rewriting tornado code

--- api_requests/index.py
+++ api_requests/index.py
@@ -40,8 +40,7 @@ class Blank(api.web.HTMLRequest):
 @handle_url(STATION_URL_REGEX)
 class StaticIndex(tornado.web.StaticFileHandler):
     def initialize(self, **kwargs) -> None:
-        self.root = ""
-        self.default_filename = ""
+        super(StaticIndex, self).initialize("", None)
 
     def get_absolute_path(self, root: str, path: str):
         return index_file_location
