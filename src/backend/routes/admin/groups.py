e837a7c07989473848e18b342a4a928aa0689d49
rmcauley <rmcauley@gmail.com>
2026-01-21 23:25:49 +0100

    Rename and turn config to python

---
+++ src/backend/routes/admin/groups.py
@@ -0,0 +1,83 @@
+import api_web.web
+from api_web.urls import handle_api_url
+from api_web import fieldtypes
+from rainwave.playlist import Song
+from rainwave.playlist import SongGroup
+
+
+@handle_api_url("admin/remove_group_from_song")
+class RemoveGroupFromSong(api_web.web.APIHandler):
+    admin_required = True
+    sid_required = False
+    description = "Removes the group from a song."
+    fields = {
+        "song_id": (fieldtypes.song_id, True),
+        "group_id": (fieldtypes.group_id, True),
+    }
+
+    def post(self):
+        s = Song.load_from_id(self.get_argument("song_id"))
+        g = SongGroup.load_from_id(self.get_argument("group_id"))
+        s.remove_group_id(g.id)
+        g.reconcile_sids()
+        self.append(
+            self.return_name,
+            {"success": "true", "tl_key": "Group removed from song ID."},
+        )
+
+
+@handle_api_url("admin/edit_group_elec_block")
+class EditGroup(api_web.web.APIHandler):
+    admin_required = True
+    sid_required = False
+    fields = {
+        "group_id": (fieldtypes.group_id, True),
+        "elec_block": (fieldtypes.zero_or_greater_integer, None),
+    }
+
+    def post(self):
+        g = SongGroup.load_from_id(self.get_argument("group_id"))
+        g.set_elec_block(self.get_argument("elec_block"))
+        self.append(
+            self.return_name,
+            {
+                "tl_key": "group_edit_success",
+                "text": "Group elec block updated to %s"
+                % self.get_argument("elec_block"),
+            },
+        )
+
+
+@handle_api_url("admin/edit_group_cooldown")
+class EditGroupCooldown(api_web.web.APIHandler):
+    admin_required = True
+    sid_required = False
+    fields = {
+        "group_id": (fieldtypes.group_id, True),
+        "cooldown": (fieldtypes.zero_or_greater_integer, True),
+    }
+
+    def post(self):
+        g = SongGroup.load_from_id(self.get_argument("group_id"))
+        g.set_cooldown(self.get_argument("cooldown"))
+        self.append(
+            self.return_name,
+            {
+                "tl_key": "group_edit_success",
+                "text": "Group cooldown updated to %s" % self.get_argument("cooldown"),
+            },
+        )
+
+
+@handle_api_url("admin/create_group")
+class CreateGroup(api_web.web.APIHandler):
+    admin_required = True
+    sid_required = False
+    fields = {"name": (fieldtypes.string, True)}
+
+    def post(self):
+        SongGroup.load_from_name(self.get_argument("name"))
+        self.append(
+            self.return_name,
+            {"tl_key": "group_create_success", "text": "Group created."},
+        )
---
+++ src/backend/routes/admin_web/scan_errors.py
@@ -0,0 +1,62 @@
+from time import time as timestamp
+import datetime
+from libs import db
+import api_web.web
+from api_web.urls import handle_url
+
+from routes.admin.scan_errors import BackendScanErrors
+
+
+def relative_time(epoch_time):
+    diff = datetime.timedelta(seconds=timestamp() - epoch_time)
+    if diff.days > 0:
+        return "%sd" % diff.days
+    elif diff.seconds > 3600:
+        return "%shr" % int(diff.seconds / 3600)
+    elif diff.seconds > 60:
+        return "%sm" % int(diff.seconds / 60)
+    elif diff.seconds > 0:
+        return "%ss" % diff.seconds
+    return "now"
+
+
+@handle_url("/admin/album_list/scan_results")
+class ScanResults(api_web.web.PrettyPrintAPIMixin, BackendScanErrors):
+    dj_preparation = True
+
+    def get(self):  # pylint: disable=E0202
+        if not isinstance(self._output, dict):
+            raise api_web.web.APIException("internal_error", http_code=500)
+
+        new_results = []
+        for row in self._output[self.return_name]:
+            if row.get("traceback"):
+                row["traceback"] = "\n".join(row["traceback"])
+                row["traceback"] = (
+                    "<pre style='max-width: 450px; overflow: auto;'>%s</pre>"
+                    % row["traceback"]
+                )
+            else:
+                row["traceback"] = " "
+            row["time"] = relative_time(row["time"])
+            new_results.append(row)
+        self._output[self.return_name] = new_results
+        super(ScanResults, self).get()
+
+
+@handle_url("/admin/tools/scan_results")
+class LatestSongs(api_web.web.HTMLRequest):
+    dj_preparation = True
+
+    def get(self):
+        self.write(self.render_string("basic_header.html", title="Latest Songs"))
+        self.write(
+            "<style type='text/css'>div { margin-bottom: 8px; border-bottom: solid 1px #888; }</style>"
+        )
+        self.write("<script>\nwindow.top.refresh_all_screens = false;\n</script>")
+
+        for fn in db.c.fetch_list(
+            "SELECT song_filename FROM r4_songs ORDER BY song_file_mtime DESC LIMIT 20"
+        ):
+            self.write("<div>%s</div>" % fn)
+        self.write(self.render_string("basic_footer.html"))
