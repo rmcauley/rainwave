fd52f070c33807c7cddc6bc0f17a774665d18dfb
rmcauley <rmcauley@gmail.com>
2026-02-05 00:50:12 +0100

    Fix random frontend sprite sheet maker throwing pylance errors

--- src/frontend/static/images/source_templates/rating_bar/sprite.py
+++ src/frontend/static/images/source_templates/rating_bar/sprite.py
@@ -8,8 +8,12 @@ for i in range(0, 26):
     user_sheet.paste(sprite.copy().crop((0, 0, 100, 23)), (0, i * 50))
     all_sheet.paste(sprite.copy().crop((0, 24, 100, 48)), (0, i * 50))
 
-user_sheet.resize((65, 780), Image.ANTIALIAS).save("bright_ldpi.png", optimize=True)
-all_sheet.resize((65, 780), Image.ANTIALIAS).save("dark_ldpi.png", optimize=True)
+user_sheet.resize((65, 780), Image.Resampling.LANCZOS).save(  # type: ignore
+    "bright_ldpi.png", optimize=True
+)
+all_sheet.resize((65, 780), Image.Resampling.LANCZOS).save(  # type: ignore
+    "dark_ldpi.png", optimize=True
+)
 
 user_sheet.crop((0, 0, 110, 1300)).save("bright_hdpi.png", optimize=True)
 all_sheet.crop((0, 0, 110, 1300)).save("dark_hdpi.png", optimize=True)
