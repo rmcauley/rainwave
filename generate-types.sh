98dbadb1e2d5a88f0672859cf62ba82f9f9ed881
rmcauley <rmcauley@gmail.com>
2026-03-16 20:53:51 +0100

    Typify translation keys

--- generate-types.sh
+++ generate-types.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+set -e
+
+tmp_openapi=$(mktemp)
+trap 'rm -f "$tmp_openapi"' EXIT
+
+jq -M --slurpfile locale lang/en_MAIN.json '
+  .components.schemas._translation_key.enum = (
+    $locale[0]
+    | to_entries
+    | map(select(.value | type=="string" or type=="array") | .key)
+    | sort
+  )
+' src/api/rainwave-openapi.json > "$tmp_openapi" && mv "$tmp_openapi" src/api/rainwave-openapi.json
+npx prettier --write ./src/api/rainwave-openapi.json
 
 uv run datamodel-codegen --input src/api/rainwave-openapi.json --input-file-type openapi --output src/api/rainwave_dto.py --output-model-type pydantic_v2.BaseModel --target-python-version 3.14 --openapi-scopes paths --formatters black --remove-special-field-name-prefix
 uv run datamodel-codegen --input src/api/rainwave-openapi.json --input-file-type openapi --output src/api/rainwave_typeddicts.py --output-model-type typing.TypedDict --target-python-version 3.14 --formatters black --remove-special-field-name-prefix
